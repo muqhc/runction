ede7ab53bf4c19af78b85b88b45c08a8f03e3c23
muqhc <muqhc07@gmail.com>
2022-01-07 11:44:24 +0900

    wrap modules

--- runction-debug/build.gradle.kts
+++ runction-debug/build.gradle.kts
@@ -1,6 +1,7 @@
 subprojects {
+    val main = "${rootProject.name}-main"
     val supportProject = "${rootProject.name}-${name.split("-")[1]}"
     dependencies {
-        implementation( rootProject.project(supportProject) )
+        implementation( rootProject.project(main).project(supportProject) )
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,16 @@
 rootProject.name = "runction"
 
 val versions = listOf(0,1)
+val main = "${rootProject.name}-main"
 val debug = "${rootProject.name}-debug"
 
+include(main)
 include(debug)
 
 versions.forEach {
     val submodule = "${rootProject.name}-$it"
     include(
-        submodule,
+        "$main:$submodule",
         "$debug:$submodule-debug"
     )
 }
